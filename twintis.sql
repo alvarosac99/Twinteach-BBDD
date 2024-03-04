53047727895307c61f4d421d45648fb4506ec865
alvarosac99 <145587906+alvarosac99@users.noreply.github.com>
2024-03-04 17:52:42 +0100

    Tablitas

--- twintis.sql
+++ twintis.sql
@@ -1,1 +1,16 @@
-a
+CREATE TABLE profesores (
+    id INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Correo VARCHAR(100),
+    aceptado BOOLEAN,
+    gestion_id INT,
+    FOREIGN KEY (gestion_id) REFERENCES gestions(id)
+);
+
+CREATE TABLE alumnos (
+    id INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Correo VARCHAR(100),
+    aceptado BOOLEAN,
+    FOREIGN KEY (id) REFERENCES profesores(id)
+);
