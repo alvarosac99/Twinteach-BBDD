8d7922040874dabc490a1434d4ff35656f5ad10d
adrianml14 <adrianml14@educastur.es>
2024-03-06 19:54:31 +0100

    Consultas que hay que hacer 1

---
+++ Consultas.sql
@@ -0,0 +1,7 @@
+--iniciar sesion / registrarse 
+--(Se puede elegir entre alumno y profesor, se rellenara la tabla de su correspondiente)
+-- Elegir cursos
+--Tienes un inventario con todo lo que conlleva
+
+--entrar en rankings creados por el profesor 
+--en los que hay tareas que dan recompensas que se añaden al inventario
