3f64686972edfe94072df824413d0848bde09fe5
adrianml14 <adrianml14@educastur.es>
2024-03-07 17:36:38 +0100

    enunciado consultas

--- Consultas.sql
+++ Consultas.sql
@@ -1,7 +1,14 @@
 --iniciar sesion / registrarse 
 --(Se puede elegir entre alumno y profesor, se rellenara la tabla de su correspondiente)
--- Elegir cursos
---Tienes un inventario con todo lo que conlleva
+-- Entras en la tabla Aumno o profesor 
+--ya dentro puedes elegir cursos, donde previamente el profesor te haya introducido
+--Tienes un inventario para cada curso con todo lo que conlleva
 
 --entrar en rankings creados por el profesor 
---en los que hay tareas que dan recompensas que se añaden al inventario
+--en los que hay tareas en las que participan alumnos
+--y que dan recompensas dependiendo de la nota en la tarea
+
+--cada vez que atacas o das una softskill te dan un cofre te llega una notificacion (con la fecha y quien lon hizo y lo que és)
+
+--una vez tengas en el inventario x item (llave), tendras acceso a mapa (tabla tierras), donde tendrás una capital, unas tierras
+--y unos recuros que se te añadiran al inventario.
